8b9e9a2e38a560084184cdc33526791fc0a68999
XU <xushicheng@163.com>
2020-02-29 18:08:40 +0800

    xushicheng@163.com

--- 09.图像融合.py
+++ 09.图像融合.py
@@ -2,7 +2,7 @@ import cv2
 
 img1 = cv2.imread('image/add/boat.bmp')
 img2 = cv2.imread('image/add/lena.bmp')
-result = cv2.addWeighted(img1, 1, img2, 1, 0)
+result = cv2.addWeighted(img1, 0.3, img2, 0.6, 0)
 cv2.imshow('img1', img1)
 cv2.imshow('img2', img2)
 cv2.imshow('cv2.addWeighted', result)
