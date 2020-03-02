336fbdf111b34a57c1f4ccba03df2fd774ce2126
XU <xushicheng@163.com>
2020-03-02 14:47:54 +0800

    xushicheng@163.com

---
+++ 1.OpenCV入门教程/23.梯度运算.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+img1 = cv2.imread('image/lena256.bmp', cv2.IMREAD_UNCHANGED)
+cv2.imshow('original', img1)
+
+img2 = cv2.morphologyEx(src=img1, op=cv2.MORPH_GRADIENT, kernel=np.ones(shape=(3, 3), dtype=np.uint8))
+cv2.imshow('cv2.morphologyEx(src=img1, op=cv2.MORPH_GRADIENT, kernel=np.ones(shape=(3, 3), dtype=np.uint8))', img2)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
