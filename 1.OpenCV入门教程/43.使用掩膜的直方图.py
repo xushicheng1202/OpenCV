9dc9c017224dc001b1d4afe126c134205b54f540
XU <xushicheng@163.com>
2020-03-05 12:33:57 +0800

    xushicheng@163.com

---
+++ 1.OpenCV入门教程/43.使用掩膜的直方图.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img1 = cv2.imread('image/lenacolor.png', cv2.IMREAD_GRAYSCALE)
+mask = np.zeros(img1.shape, np.uint8)
+mask[200:400, 200:400] = 255
+histO = cv2.calcHist(images=[img1], channels=[0], mask=None, histSize=[256], ranges=[0, 255])
+histM = cv2.calcHist(images=[img1], channels=[0], mask=mask, histSize=[256], ranges=[0, 255])
+
+# plt.plot(histO)
+# plt.plot(histM)
+# 无法显示图片，plt.plot()，方式有问题
