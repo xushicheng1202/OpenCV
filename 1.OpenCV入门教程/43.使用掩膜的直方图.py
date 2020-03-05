273fc8493835bf3924ac5c83f4e9951a8a1307dc
XU <xushicheng@163.com>
2020-03-05 14:44:33 +0800

    xushicheng@163.com

--- 1.OpenCV入门教程/43.使用掩膜的直方图.py
+++ 1.OpenCV入门教程/43.使用掩膜的直方图.py
@@ -8,6 +8,12 @@ mask[200:400, 200:400] = 255
 histO = cv2.calcHist(images=[img1], channels=[0], mask=None, histSize=[256], ranges=[0, 255])
 histM = cv2.calcHist(images=[img1], channels=[0], mask=mask, histSize=[256], ranges=[0, 255])
 
+cv2.imshow('original', histO)
+cv2.imshow('result', histM)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
 # plt.plot(histO)
 # plt.plot(histM)
 # 无法显示图片，plt.plot()，方式有问题
