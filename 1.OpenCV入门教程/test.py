887e03def4a7fe452d59856d54e950c2e94b9911
XU <xushicheng@163.com>
2020-03-07 20:50:29 +0800

    xushicheng@163.com

--- 1.OpenCV入门教程/test.py
+++ 1.OpenCV入门教程/test.py
@@ -1,15 +1,14 @@
 import cv2 as cv
 
 face_cascade = cv.CascadeClassifier(
-    'C:\ProgramData\Anaconda3\envs\OpenCV\Library\etc\haarcascades\haarcascade_frontalface_default.xml')
-eye_cascade = cv.CascadeClassifier('C:\ProgramData\Anaconda3\envs\OpenCV\Library\etc\haarcascades\haarcascade_eye.xml')
+    'C:\ProgramData\Anaconda3\envs\OpenCV\Library\etc\haarcascades\haarcascade_frontalface_alt.xml')
 
-img = cv.imread('image/lenacolor.png')
+img = cv.imread('image/timg.jpg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
 for (x, y, w, h) in faces:
-    cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     roi_gray = gray[y:y + h, x:x + w]
     roi_color = img[y:y + h, x:x + w]
 
